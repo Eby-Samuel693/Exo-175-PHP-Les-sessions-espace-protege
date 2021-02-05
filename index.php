264ce88439e2a4e70cdc3eafe3ff18b647c90aa5
Eby-Samuel693 <coquelet.samuel@gmail.com>
2021-02-05 11:24:06 +0100

    jhgfds

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <?php
+        if (isset($_POST['mot_de_passe']) AND $_POST['mot_de_passe'] ==  "SamSoul") // Si le mot de passe est bon
+        {
+            // On affiche les codes
+            ?>
+            <h1>bienjoue</h1>
+            <p>j'ai mis  3h pour cette exo :( </p>
+            <p>ci logique pourtant</p>
+            <?php
+        }
+        else // Sinon, on affiche un message d'erreur
+        {
+            echo '<p>Mot de passe incorrect</p>';
+        }
+        ?>
+    </div>
+</body>
+</html>
---
+++ page1.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <p>Veuillez entrer le mot de passe :</p>
+    <form action="index.php" method="post">
+        <p>
+            <input type="password" name="mot_de_passe" />
+            <input type="submit" value="Valider" />
+        </p>
+    </form>
+
+</body>
+</html>
